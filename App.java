e2e7eb25ed4e29fdee8ae4cef74707c9fb687f60
Silac1995 <59840327+Silac1995@users.noreply.github.com>
2021-07-15 02:11:11 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,54 @@
+
+import controller.ControllerCalculator;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		@SuppressWarnings("unused")
+		ControllerCalculator calculatorFrame = new ControllerCalculator();
+	}
+
+//		Scanner input = new Scanner(System.in);
+//		int choice = 0;
+//		int number1 = 0;
+//		int number2 = 0;
+//
+//		ControllerCalculator calculatrice = new ControllerCalculator();
+//
+//		System.out.print("Make your choice:\n1.Additionner\n2.Soustraire\n3.Multiplier\n4.Diviser");
+//		choice = input.nextInt();
+//
+//		System.out.print(choice);
+//
+////		switch (choice) {
+////		case 1:
+////			System.out.println("Saisir les deux nombres:");
+////			number1 = input.nextInt();
+////			number2 = input.nextInt();
+////			calculatrice.getModelCalculator().addition(number1, number2);
+////			System.out.println(number1 + " + " + number2 + " = " + calculatrice.getModelCalculator().getResults());
+////			break;
+////
+////		case 2:
+////			System.out.println("Saisir les deux nombres:");
+////
+////			number1 = input.nextInt();
+////			number2 = input.nextInt();
+////			break;
+////
+////		case 3:
+////			System.out.println("Saisir les deux nombres:");
+////
+////			number1 = input.nextInt();
+////			number2 = input.nextInt();
+////			break;
+////
+////		case 4:
+////			System.out.println("Saisir les deux nombres:");
+////
+////			number1 = input.nextInt();
+////			number2 = input.nextInt();
+////			break;
+////		}
+}
